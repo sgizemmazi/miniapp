d49231080b75665e8cce20c6e5276860e9ab9f73
sgizemmazi <117825170+sgizemmazi@users.noreply.github.com>
2022-12-12 11:12:13 +0200

    ngModel yüklendi.

--- first-study/src/app/app.module.ts
+++ first-study/src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgModelComponent } from './directives/ng-model/ng-model.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgModelComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ first-study/src/app/directives/ng-model/ng-model.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-model',
+  templateUrl: './ng-model.component.html',
+  styleUrls: ['./ng-model.component.css']
+  
+})
+export class NgModelComponent implements OnInit{
+name:string="gizem";
+constructor() {
+  
+}
+  ngOnInit(): void {
+  
+}
+}
