163e41100dadf4046cb54edcb556758e3c37fdf4
sgizemmazi <117825170+sgizemmazi@users.noreply.github.com>
2022-12-18 13:43:29 +0200

    düzenleme

--- first-study/src/app/app.module.ts
+++ first-study/src/app/app.module.ts
@@ -4,13 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-//import { NgModelComponent } from './directives/ng-model/ng-model.component';
 import { TwoOneWayDataBindingComponent } from './two-one-way-data-binding/two-one-way-data-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-   // NgModelComponent,
     TwoOneWayDataBindingComponent
   ],
   imports: [
